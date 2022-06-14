2fe3940cd75d940e2115c5ede37836279f569f9d
Tariel <tarieltitirashvili@gmail.com>
2022-06-14 03:08:42 -0700

    rendering pizzas by db.json

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 
 type Props = {};
+const categories: string[] = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
 const Categories = (props: Props) => {
+  const [selectedCategory, setSelectedCategory] = React.useState<string>('');
+
+  const onClickNewCategory = (e: React.MouseEvent<HTMLLIElement, MouseEvent>, name: string) => {
+    setSelectedCategory(name);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((category) => (
+          <li
+            key={category}
+            onClick={(e) => onClickNewCategory(e, category)}
+            className={selectedCategory === category ? 'active' : ''}>
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/components/PizzaBlock.tsx
+++ src/components/PizzaBlock.tsx
@@ -1,30 +1,48 @@
 import React from 'react';
 
-type Props = {};
+type Props = {
+  price: number;
+  title: string;
+  imageUrl: string;
+  sizes: number[];
+  types: number[];
+};
 
 const PizzaBlock = (props: Props) => {
+  const { price, title, imageUrl, sizes, types } = props;
+  const typeNames: string[] = ['тонкое', 'традиционное'];
+  const [selectedType, setSelectedType] = React.useState<number>(types[0]);
+  const [selectedSize, setSelectedSize] = React.useState<number>(sizes[0]);
+
   return (
     <div className="pizza-block">
-      <img
-        className="pizza-block__image"
-        src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-        alt="Pizza"
-      />
-      <h4 className="pizza-block__title">Чизбургер-пицца</h4>
+      <img className="pizza-block__image" src={imageUrl} alt={title} />
+      <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>
-          <li className="active">тонкое</li>
-          <li>традиционное</li>
+          {types.map((type) => (
+            <li
+              onClick={() => setSelectedType(type)}
+              className={selectedType === type ? 'active' : ''}
+              key={typeNames[type]}>
+              {typeNames[type]}
+            </li>
+          ))}
         </ul>
         <ul>
-          <li className="active">26 см.</li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {sizes.map((size) => (
+            <li
+              key={size}
+              onClick={() => setSelectedSize(size)}
+              className={selectedSize === size ? 'active' : ''}>
+              {size} см.
+            </li>
+          ))}
         </ul>
       </div>
       <div className="pizza-block__bottom">
-        <div className="pizza-block__price">от 395 ₽</div>
-        <div className="button button--outline button--add">
+        <div className="pizza-block__price">от {price} ₽</div>
+        <button className="button button--outline button--add">
           <svg
             width="12"
             height="12"
@@ -37,8 +55,8 @@ const PizzaBlock = (props: Props) => {
             />
           </svg>
           <span>Добавить</span>
-          <i>2</i>
-        </div>
+          <i>0</i>
+        </button>
       </div>
     </div>
   );
