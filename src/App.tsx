1983e0e0c9b522b8296bc27df3bac489543d5baa
Tariel <tarieltitirashvili@gmail.com>
2022-06-14 15:37:54 -0700

    get pizzas from mockAPI

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,31 @@ import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
 import Sort from './components/Sort';
 import './scss/app.scss';
-import pizzas from './assets/db/db.json';
+
+interface Pizza {
+  id: number;
+  imageUrl: string;
+  title: string;
+  types: number[];
+  sizes: number[];
+  price: number;
+  category: number;
+  rating: number;
+}
 
 function App() {
+  const [pizzas, setPizzas] = React.useState<Pizza[]>([]);
+  const getData = (): void => {
+    fetch('https://62a85ee7943591102ba05a2c.mockapi.io/pizzas')
+      .then((data) => data.json())
+      .then((data) => {
+        setPizzas(data);
+        console.log(data);
+      });
+  };
+  React.useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="wrapper">
       <Header />
@@ -18,7 +40,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.pizzas.map((pizza) => (
+            {pizzas.map((pizza: Pizza) => (
               <PizzaBlock key={pizza.id} {...pizza} />
             ))}
           </div>
