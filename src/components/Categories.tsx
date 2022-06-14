2368428b3683e8862bc1477e13a2129d091e37c4
Tariel <tarieltitirashvili@gmail.com>
2022-06-13 19:37:02 -0700

    markup for listing page

---
+++ src/components/Categories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+type Props = {};
+
+const Categories = (props: Props) => {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Все</li>
+        <li>Мясные</li>
+        <li>Вегетарианская</li>
+        <li>Гриль</li>
+        <li>Острые</li>
+        <li>Закрытые</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
